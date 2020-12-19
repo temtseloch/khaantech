6a0e4ebe64a32cc23bf112c2aca546bcd61b95b3
Temtsel-Och Ulziisaikhan <temkalways@gmai.com>
2020-12-19 10:55:10 -0500

    onclick info

--- app.js
+++ app.js
@@ -120,23 +120,23 @@ function ajax(method, url, data, success, error) {
 // INFO
 
 document.querySelector(".temtsel-image").addEventListener("click", function () {
-  document.querySelector(".temtsel-image").style.display = "none";
-  document.querySelector(".temtsel-info").style.display = "block";
-});
-
-document.querySelector(".temtsel-info").addEventListener("click", function () {
-  document.querySelector(".temtsel-info").style.display = "none";
-  document.querySelector(".temtsel-image").style.display = "block";
+  if (document.querySelector(".elbeg-image").style.display === "none") {
+    document.querySelector(".temtsel-info").style.display = "none";
+    document.querySelector(".elbeg-image").style.display = "block";
+  } else {
+    document.querySelector(".elbeg-image").style.display = "none";
+    document.querySelector(".temtsel-info").style.display = "block";
+  }
 });
 
 document.querySelector(".elbeg-image").addEventListener("click", function () {
-  document.querySelector(".elbeg-image").style.display = "none";
-  document.querySelector(".elbeg-info").style.display = "block";
-});
-
-document.querySelector(".elbeg-info").addEventListener("click", function () {
-  document.querySelector(".elbeg-info").style.display = "none";
-  document.querySelector(".elbeg-image").style.display = "block";
+  if (document.querySelector(".temtsel-image").style.display === "none") {
+    document.querySelector(".elbeg-info").style.display = "none";
+    document.querySelector(".temtsel-image").style.display = "block";
+  } else {
+    document.querySelector(".temtsel-image").style.display = "none";
+    document.querySelector(".elbeg-info").style.display = "block";
+  }
 });
 
 // LANGUAGE SWITCH
